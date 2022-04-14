f7a00194adb37b5268c246a44e08eb689aeb850b
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-14 15:22:59 +0100

    added to logic

--- src/index.js
+++ src/index.js
@@ -62,28 +62,39 @@ function same(arr1, arr2) {
   obj1 = {};
   obj2 = {};
 
-  if (typeof arr1 === "number" && typeof arr2 === "number") {
-    for (let i = 0; i < arr1.length; i++) {
+  let char1 = arr1.toString();
+  console.log(char1);
+  let char2 = arr2.toString();
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (typeof arr1 === "number") {
       let key1 = arr1[i];
       if (obj1[key1] > 0) {
-        obj1[key1];
+        obj1[key1] += 1;
+      } else {
+        obj1[key1] = 1;
       }
     }
+  
 
-    for (let j = 0; j < arr2.length; j++) {
+  for (let j = 0; j < arr2.length; j++) {
+    if (typeof arr2 === "number") {
       let key2 = arr2[j];
       if (obj2[key2] > 0) {
-        obj2[key2];
+        obj2[key2] += 1;
+      } else {
+        obj2[key2] = 1;
       }
     }
+  
 
-    for (key in obj1) {
-      if (obj2[key] !== obj1[key] * obj1[key]) {
-        return false;
-      }
-      return true;
+  for (let key in obj1) {
+    if (obj2[key] !== obj1[key] * obj1[key]) {
+      return false;
     }
+    return true;
   }
 }
 
-same([1, 2, 3], [4, 1, 9]);
+  
+same([1, 2, 3], [4, 1, 9])
